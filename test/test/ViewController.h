7a88c9aaebcfa2121967f41fc9f2f68bc4f0297c
“hiyodo” <a7689999@126.com>
2016-06-19 09:32:40 +0800

    test1

---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by tarenayj on 16/6/19.
+//  Copyright © 2016年 TR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by tarenayj on 16/6/19.
+//  Copyright © 2016年 TR. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
